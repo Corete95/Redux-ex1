256d708a24335a36f36651f0f26af6a287e6b219
Corete <fanthell95@gmail.com>
2021-01-02 20:09:43 +0900

    커밋확인

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Root</h1>
+        <h1>Root!!</h1>
         <AddNumberRoot></AddNumberRoot>
         <DisplayNumberRoot></DisplayNumberRoot>
       </div>
